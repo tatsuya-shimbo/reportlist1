5231405e95890d16aede0726ad970d728ae05209
tatsuya.shimbo <tatsuya.s0131@gmail.com>
2019-10-01 15:36:35 +0000

    user signup

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase!}
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  validates :university, presence: true, length: { maximum: 50 }
+  validates :entrance_year, length: { is: 4 }
+  
+  has_secure_password
+end
