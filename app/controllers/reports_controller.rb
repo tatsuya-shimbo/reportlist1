2707ebbb4fae161488437a7f21521a06601853dc
tatsuya.shimbo <tatsuya.s0131@gmail.com>
2019-12-17 10:14:41 +0000

    aside_sort

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -103,10 +103,14 @@ class ReportsController < ApplicationController
   end
   
   def reportlist
-    @reportlist_1 = current_user.reports.where(["day >= :day_s and day < :day_e", {day_s: current_user.entrance_year * 10000 + 401, day_e: current_user.entrance_year * 10000 + 10401}])
-    @reportlist_2 = current_user.reports.where(["day >= :day_s and day < :day_e", {day_s: current_user.entrance_year * 10000 + 10401, day_e: current_user.entrance_year * 10000 + 20401}])
-    @reportlist_3 = current_user.reports.where(["day >= :day_s and day < :day_e", {day_s: current_user.entrance_year * 10000 + 20401, day_e: current_user.entrance_year * 10000 + 30401}])
-    @reportlist_4 = current_user.reports.where(["day >= :day_s and day < :day_e", {day_s: current_user.entrance_year * 10000 + 30401, day_e: current_user.entrance_year * 10000 + 40401}])
+    reportlist_1 = current_user.reports.where(["day >= :day_s and day < :day_e", {day_s: current_user.entrance_year * 10000 + 401, day_e: current_user.entrance_year * 10000 + 10401}])
+    reportlist_2 = current_user.reports.where(["day >= :day_s and day < :day_e", {day_s: current_user.entrance_year * 10000 + 10401, day_e: current_user.entrance_year * 10000 + 20401}])
+    reportlist_3 = current_user.reports.where(["day >= :day_s and day < :day_e", {day_s: current_user.entrance_year * 10000 + 20401, day_e: current_user.entrance_year * 10000 + 30401}])
+    reportlist_4 = current_user.reports.where(["day >= :day_s and day < :day_e", {day_s: current_user.entrance_year * 10000 + 30401, day_e: current_user.entrance_year * 10000 + 40401}])
+    @reportlist_1 = reportlist_1.order(:day)
+    @reportlist_2 = reportlist_2.order(:day)
+    @reportlist_3 = reportlist_3.order(:day)
+    @reportlist_4 = reportlist_4.order(:day)
   end
 end
 
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,7 @@ class Report < ApplicationRecord
   
   has_one_attached :repo
   
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :day, presence: true, length: { is: 8 }
   validates :teaching, length: { maximum: 50 }
